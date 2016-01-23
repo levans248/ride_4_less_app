2eb0b35d089e7091ad9e75ceed5a6a839ec94d31
Luke Evans <luevans248@gmail.com>
2016-01-23 12:58:05 -0800

    added config.assets.initialize_on_precompile = false to application.rb file for geocoder gem to work on heroku.'

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Ride4LessApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.initialize_on_precompile = false
   end
 end
